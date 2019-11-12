d23552dc3f45f7d4c7d99c877cfb4675ffea7512
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-12 23:32:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,9 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.each {|hero|"#{hero.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
