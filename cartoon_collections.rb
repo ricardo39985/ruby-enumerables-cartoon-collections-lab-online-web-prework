d1d0b15e0c297c055e51a820a84c06d534a56fb7
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-12 23:51:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def long_planeteer_calls(array)# code an argument here
   # Your code here
   result = array.select { |e| e.length > 4 }
-
+  result
 end
 
 def find_the_cheese# code an argument here
