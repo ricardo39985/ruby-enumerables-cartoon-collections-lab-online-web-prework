35e468a9c2914eb590cbdf15654edd16d8dbcf29
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-12 23:50:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,10 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map {|n| "#{n.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  result = array.select { |e| e.length > 4 }
+
 end
 
 def find_the_cheese# code an argument here
